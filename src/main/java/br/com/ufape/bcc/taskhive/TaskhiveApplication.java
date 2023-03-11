ec621f9ff40a62fb4014cc15a9e6b7bf9309f443
Sara Abreu <abreusara012@gmail.com>
2023-03-11 12:26:32 -0300

    Initial Commit

---
+++ src/main/java/br/com/ufape/bcc/taskhive/TaskhiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufape.bcc.taskhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskhiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ufape/bcc/taskhive/TaskhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufape.bcc.taskhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
