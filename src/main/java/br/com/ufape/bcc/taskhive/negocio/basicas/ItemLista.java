5bd9c30285d607b20cc43949473e2420325a0be3
ynjisng <abreusara012@gmail.com>
2023-03-29 13:46:07 -0300

    seta itemlista como entidade

--- src/main/java/br/com/ufape/bcc/taskhive/negocio/basicas/ItemLista.java
+++ src/main/java/br/com/ufape/bcc/taskhive/negocio/basicas/ItemLista.java
@@ -1,7 +1,17 @@
 package br.com.ufape.bcc.taskhive.negocio.basicas;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ItemLista {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    
     private String item;
     private boolean checked;
     
