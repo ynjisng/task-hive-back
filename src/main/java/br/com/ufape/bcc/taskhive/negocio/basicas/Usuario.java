79eb1219d5939814022e2b193e43eae82b58d65d
Ricardo <24703825+RickyM7@users.noreply.github.com>
2023-04-02 23:39:12 +0000

    Atualização da classe Usuario (#8)

--- src/main/java/br/com/ufape/bcc/taskhive/negocio/basicas/Usuario.java
+++ src/main/java/br/com/ufape/bcc/taskhive/negocio/basicas/Usuario.java
@@ -1,5 +1,50 @@
 package br.com.ufape.bcc.taskhive.negocio.basicas;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario {
-    
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+    //metodos
+    //construtores
+    public Usuario() {}
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+    //getters and setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
