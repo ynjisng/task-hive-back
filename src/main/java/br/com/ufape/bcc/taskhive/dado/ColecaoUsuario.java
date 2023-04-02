f511d3868c777ba073f79d456245604cbc987cf4
Ricardo <24703825+RickyM7@users.noreply.github.com>
2023-04-02 23:40:29 +0000

    Upload de arquivos da pasta dados (#11)

---
+++ src/main/java/br/com/ufape/bcc/taskhive/dado/ColecaoUsuario.java
@@ -0,0 +1,11 @@
+package br.com.ufape.bcc.taskhive.dado;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.ufape.bcc.taskhive.negocio.basicas.Usuario;
+
+@Repository
+public interface ColecaoUsuario extends JpaRepository<Usuario, Long> {
+    public Usuario findByEmail(String email);
+}
