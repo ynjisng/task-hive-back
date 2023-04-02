b8fcc3234449444fc759f6d571e1a772e994ecd5
Ricardo <24703825+RickyM7@users.noreply.github.com>
2023-04-02 23:30:41 +0000

    Upload de arquivos da pasta cadastro

---
+++ src/main/java/br/com/ufape/bcc/taskhive/negocio/cadastro/CadastroProjeto.java
@@ -0,0 +1,45 @@
+package br.com.ufape.bcc.taskhive.negocio.cadastro;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ufape.bcc.taskhive.dado.ColecaoProjeto;
+import br.com.ufape.bcc.taskhive.negocio.basicas.Projeto;
+
+@Service
+public class CadastroProjeto implements InterfaceCadastroProjeto {
+    @Autowired
+    private ColecaoProjeto colecaoProjeto;
+
+    @Override
+    public List<Projeto> procurarProjetoNome(String nome) {
+        return colecaoProjeto.findByNomeContaining(nome);
+    }
+
+    @Override
+    public Projeto procurarProjetoId(long id) {
+        return colecaoProjeto.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Projeto> listarProjetos() {
+        return colecaoProjeto.findAll();
+    }
+
+    @Override
+    public void deletarProjetoId(long id) {
+        colecaoProjeto.deleteById(id);
+    }
+
+    @Override
+    public Projeto salvarProjeto(Projeto entity) {
+        return colecaoProjeto.save(entity);
+    }
+
+    @Override
+    public void deletarProjeto(Projeto projeto) {
+        colecaoProjeto.delete(projeto);
+    }
+}
